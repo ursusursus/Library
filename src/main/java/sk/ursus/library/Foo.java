8399fb4ffb02362d6dad07064c001ecb2101fbf8
Vlastimil Brečka <vlastimil@vlastimilbrecka.sk>
2017-12-08 18:54:46 +0100

    first commit

---
+++ src/main/java/sk/ursus/library/Foo.java
@@ -0,0 +1,12 @@
+package sk.ursus.library;
+
+/**
+ * Created by Vlastimil Brečka (www.vlastimilbrecka.sk)
+ * on 08-Dec-17.
+ */
+public class Foo {
+
+    public static String foo() {
+        return "adasdas";
+    }
+}
